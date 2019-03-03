63a5c1e9a756125ed91b29432f20af65a7a8e88d
littlekhung <little_tor@hotmail.com>
2019-03-03 21:28:34 +0700

    Thai language initiate and test sign up

--- app/src/main/java/com/example/wireless_gradecalculation/MainActivity.java
+++ app/src/main/java/com/example/wireless_gradecalculation/MainActivity.java
@@ -2,12 +2,71 @@ package com.example.wireless_gradecalculation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private Button signin;
+    private TextView user;
+    private TextView pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Toast.makeText(MainActivity.this, "sign", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "not sign", Toast.LENGTH_SHORT).show();
+                }
+                // ...
+            }
+        };
+        signin = (Button)findViewById(R.id.signin);
+        user = (TextView)findViewById(R.id.username);
+        pass = (TextView)findViewById(R.id.password);
+        signin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mAuth.createUserWithEmailAndPassword(user.getText().toString(), pass.getText().toString())
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>(){
+                            @Override
+                            public void onComplete(Task<AuthResult> task) {
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                }
+                                // ...
+                            }
+                        });
+            }
+        });
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
