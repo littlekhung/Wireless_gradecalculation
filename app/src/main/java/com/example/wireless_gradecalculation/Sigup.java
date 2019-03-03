0147db977731a3ab2535a983e3137612baa716f5
Kramolneat <34006150+Kramolneat@users.noreply.github.com>
2019-03-03 22:29:35 +0700

    add signup (love love love)

---
+++ app/src/main/java/com/example/wireless_gradecalculation/Sigup.java
@@ -0,0 +1,13 @@
+package com.example.wireless_gradecalculation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sigup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sigup);
+    }
+}
