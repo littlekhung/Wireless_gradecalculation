2b272daf73e332f0fc5b86006dacc7d40b3facd1
littlekhung <little_tor@hotmail.com>
2019-03-04 09:56:48 +0700

    adjust layout to prevent error change "username" -> "email"

--- app/src/main/java/com/example/wireless_gradecalculation/MainActivity.java
+++ app/src/main/java/com/example/wireless_gradecalculation/MainActivity.java
@@ -15,7 +15,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth = FirebaseAuth.getInstance();;
+    private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private Button login;
     private Button signUp;
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(FirebaseAuth firebaseAuth) {
