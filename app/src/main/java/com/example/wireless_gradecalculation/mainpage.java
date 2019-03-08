bbd54de6c640b5a201bd50ab7e6f6a919a9bf2cc
Kramolneat <34006150+Kramolneat@users.noreply.github.com>
2019-03-08 11:53:56 +0700

    add main page which has name photo grade spinner

---
+++ app/src/main/java/com/example/wireless_gradecalculation/mainpage.java
@@ -0,0 +1,25 @@
+package com.example.wireless_gradecalculation;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class mainpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainpage);
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(mainpage.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.education_level));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
+
+    }
+}
