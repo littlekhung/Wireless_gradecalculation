9cc94606626ccad4f33cad7797d714c1bf33b3e9
Kramolneat <34006150+Kramolneat@users.noreply.github.com>
2019-03-26 14:17:25 +0700

    Add setting page

--- app/src/main/java/com/example/wireless_gradecalculation/Setting.java
+++ app/src/main/java/com/example/wireless_gradecalculation/Setting.java
@@ -1,13 +1,48 @@
 package com.example.wireless_gradecalculation;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Setting extends AppCompatActivity {
+import com.akexorcist.localizationactivity.ui.LocalizationActivity;
 
+public class Setting extends LocalizationActivity {
+
+    private Dialog myDialog;
+    private TextView changepass;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        setTitle(R.string.app_name);
+        myDialog = new Dialog(this);
+        changepass = (TextView) findViewById(R.id.changePass);
+        changepass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowPopup();
+            }
+        });
+
+    }
+    public void ShowPopup() {
+        TextView txtclose;
+        Button complete;
+        myDialog.setContentView(R.layout.popup);
+        txtclose =(TextView) myDialog.findViewById(R.id.txtclose);
+        complete = (Button) myDialog.findViewById(R.id.complete);
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
     }
 }
