4b403def606a7689bd8fec19a3ceac18e6b200b8
littlekhung <little_tor@hotmail.com>
2019-03-28 19:31:14 +0700

    Add camera access

--- app/src/main/java/com/example/wireless_gradecalculation/Setting.java
+++ app/src/main/java/com/example/wireless_gradecalculation/Setting.java
@@ -1,13 +1,22 @@
 package com.example.wireless_gradecalculation;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.Dialog;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.drawable.ColorDrawable;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +25,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.akexorcist.localizationactivity.ui.LocalizationActivity;
 import com.google.firebase.auth.FirebaseAuth;
@@ -34,6 +44,9 @@ public class Setting extends LocalizationActivity {
     private TextView changephoto;
     private FirebaseAuth mAuth;
     private TextView Camera;
+    private Uri imageUri;
+    public static final int TAKEPICTURE = 666;
+    public static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 123;
     ImageView image;
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -94,10 +107,19 @@ public class Setting extends LocalizationActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap =(Bitmap)data.getExtras().get("data");
-        bitmap.createScaledBitmap(bitmap,160,160,false);
+
+        if(requestCode == TAKEPICTURE && resultCode == Activity.RESULT_OK){
+            try {
+                Bitmap pic = MediaStore.Images.Media.getBitmap(getContentResolver(),imageUri);
+                pic = rotateImage(pic,270);
+                pic.createScaledBitmap(pic,160,160,false);
+                image.setImageBitmap(pic);
+
+            }catch (Exception e){
+
+            }
+        }
 //        ByteArrayOutputStream bytes = new ByteArrayOutputStream();.s);
-        image.setImageBitmap(bitmap);
 
     }
     ////popforcamera
@@ -107,12 +129,11 @@ public class Setting extends LocalizationActivity {
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View convertView = inflater.inflate(R.layout.popupforcamera, null);
         Camera =(TextView) myDialogCam.findViewById(R.id.camera);
-
+        //https://stackoverflow.com/questions/47027264/overcoming-photo-results-from-android-cameras-that-produce-low-resolution?rq=1
         Camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,0);
+                takePicturePermission();
             }
         });
         TextView txtclose;
@@ -126,5 +147,66 @@ public class Setting extends LocalizationActivity {
         });
         myDialogCam.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         myDialogCam.show();
+    }
+    void takePicture(){
+        ContentValues cv = new ContentValues();
+        cv.put(MediaStore.Images.Media.TITLE,"ProfilePicture");
+        cv.put(MediaStore.Images.Media.DESCRIPTION,"");
+        imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,cv);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+        startActivityForResult(intent,TAKEPICTURE);
+        myDialogCam.dismiss();
+    }
+    //from https://stackoverflow.com/questions/14066038/why-does-an-image-captured-using-camera-intent-gets-rotated-on-some-devices-on-a?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
+    }
+    ///////////////////
+    ////// from https://developer.android.com/training/permissions/requesting.html#java
+    private void takePicturePermission(){
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            //request the permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+            }
+        else {
+            // Permission has already been granted
+            takePicture();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    takePicture();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this,"Need Permission!!!",Toast.LENGTH_SHORT);
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
     }
 }
