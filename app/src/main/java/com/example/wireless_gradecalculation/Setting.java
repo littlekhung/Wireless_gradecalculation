8da4a905624df96fe834c5cdabfd91c8d0d6bdee
Kramolneat <34006150+Kramolneat@users.noreply.github.com>
2019-03-28 18:16:24 +0700

    Camera added ver2-3 hule we can change photo

--- app/src/main/java/com/example/wireless_gradecalculation/Setting.java
+++ app/src/main/java/com/example/wireless_gradecalculation/Setting.java
@@ -10,6 +10,7 @@ import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -21,11 +22,14 @@ import com.google.firebase.auth.FirebaseAuth;
 
 import org.w3c.dom.Text;
 
+import java.io.ByteArrayOutputStream;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Setting extends LocalizationActivity {
     private TextView logoutButton;
     private Dialog myDialog;
+    private Dialog myDialogCam;
     private TextView changepass;
     private TextView changephoto;
     private FirebaseAuth mAuth;
@@ -38,6 +42,9 @@ public class Setting extends LocalizationActivity {
         setTitle(R.string.app_name);
         mAuth = FirebaseAuth.getInstance();
         myDialog = new Dialog(this);
+        myDialogCam = new Dialog(this);
+        image = (ImageView) findViewById(R.id.ima);
+
         changephoto = (TextView) findViewById(R.id.changePhoto);
         changephoto.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,19 +69,7 @@ public class Setting extends LocalizationActivity {
                 finish();
             }
         });
-        ///Camera//
-        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View convertView = inflater.inflate(R.layout.popupforcamera, null);
-        Camera =(TextView) convertView.findViewById(R.id.camera);
-        image = (ImageView) findViewById(R.id.ima);
 
-        Camera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,0);
-            }
-        });
 
 
     }
@@ -100,23 +95,36 @@ public class Setting extends LocalizationActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         Bitmap bitmap =(Bitmap)data.getExtras().get("data");
+        bitmap.createScaledBitmap(bitmap,160,160,false);
+//        ByteArrayOutputStream bytes = new ByteArrayOutputStream();.s);
         image.setImageBitmap(bitmap);
 
     }
     ////popforcamera
     public void ShowPopupCamera() {
+        myDialogCam.setContentView(R.layout.popupforcamera);
+        ///Camera//
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View convertView = inflater.inflate(R.layout.popupforcamera, null);
+        Camera =(TextView) myDialogCam.findViewById(R.id.camera);
+
+        Camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
         TextView txtclose;
-        Button complete;
-        myDialog.setContentView(R.layout.popup);
-        txtclose =(TextView) myDialog.findViewById(R.id.txtclose);
-        complete = (Button) myDialog.findViewById(R.id.complete);
+
+        txtclose =(TextView) myDialogCam.findViewById(R.id.txtclose);
         txtclose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myDialog.dismiss();
+                myDialogCam.dismiss();
             }
         });
-        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        myDialog.show();
+        myDialogCam.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialogCam.show();
     }
 }
